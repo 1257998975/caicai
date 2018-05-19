245bdcb8a033288e6c8d14ddddc9586b9b7bc44b
xx <1257998975@QQ.COM>
2018-05-19 10:55:44 +0800

    create

--- caicaipython/settings.py
+++ caicaipython/settings.py
@@ -77,11 +77,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
+        'HOST': '39.106.19.189',
         'PORT': '3306',
-        'NAME': 'book',
+        'NAME': 'caicai',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': 'caicai',
     }
 }
 
