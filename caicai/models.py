29968e6059484cb151f8a9757f848b17730d6ff4
xx <1257998975@QQ.COM>
2018-05-21 08:35:21 +0800

    create

--- caicai/models.py
+++ caicai/models.py
@@ -3,15 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 
 class Users(models.Model):
-    User_id=models.CharField(max_length=20,primary_key=True)
-    Password=models.CharField(max_length=20)
-    Wechatid = models.CharField(max_length=20,blank=True)
+    User_id = models.CharField(max_length=20, primary_key=True)
+    Password = models.CharField(max_length=20)
+    Wechatid = models.CharField(max_length=20, blank=True)
+
 
 class Admins(models.Model):
-    Adm_account = models.CharField(max_length=20,primary_key=True)
+    Adm_account = models.CharField(max_length=20, primary_key=True)
     Adm_password = models.CharField(max_length=20)
     Adm_openid = models.CharField(max_length=20)
 
@@ -25,13 +27,32 @@ class User_infor(models.Model):
 
 
 class Goods_car(models.Model):
-    User_id=models.CharField(max_length=20,primary_key=True)
-    Goods_id=models.CharField(max_length=10)
+    User_id = models.CharField(max_length=20, primary_key=True)
+    Goods_id = models.CharField(max_length=10)
     Order_id = models.CharField(max_length=10)
 
 
 class Order(models.Model):
-    User_id=models.CharField(max_length=20,primary_key=True)
-    Goods_id=models.CharField(max_length=10)
+    User_id = models.CharField(max_length=20, primary_key=True)
+    Goods_id = models.CharField(max_length=10)
     Order_id = models.CharField(max_length=10)
 
+
+class caicai(models.Model):
+    Goods_id = models.CharField(max_length=20, primary_key=True)
+    Goods_name = models.CharField(max_length=20)
+    Goods_price = models.DecimalField(max_digits=10, decimal_places=2)
+    Discount = models.DecimalField(max_digits=10, decimal_places=2)
+    Goods_picture = models.CharField(max_length=20)
+    Reserves = models.DecimalField(max_digits=10, decimal_places=2)
+    Goods_location = models.CharField(max_length=20)
+    Goods_count = models.DecimalField(max_digits=10, decimal_places=2)
+    Goods_states = models.IntegerField()
+
+
+class UserRecord(models.Model):
+    id = models.AutoField()
+    User_id = models.CharField(max_length=20)
+    Address =  models.CharField(max_length=20)
+    Tel =  models.CharField(max_length=20)
+    Name = models.CharField(max_length=20)
